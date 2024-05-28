bf79bf2eb1787d721876c570bc8996692b0c6646
Shivasai22 <prasadshivasai22@gmail.com>
2024-05-28 12:47:22 +0530

    modified:   nonprimitive.java

--- nonprimitive.java
+++ nonprimitive.java
@@ -1,4 +1,5 @@
 public class nonprimitive {
+
     interface animal {
         void makeSound();
     }
@@ -7,6 +8,16 @@ public class nonprimitive {
         public void makeSound() {
             System.out.println("Bark");
         }
+    }
 
+    public static void main(String[] args) {
+        // Create an instance of the outer class
+        nonprimitive outer = new nonprimitive();
+
+        // Use the outer class instance to create an instance of the inner class
+        Dog myDog = outer.new Dog();
+
+        // Call the makeSound() method on the object
+        myDog.makeSound();
     }
 }
