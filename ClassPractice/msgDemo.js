46c7053610af0de260c4fea002d0c418f63c276e
Shivasai22 <prasadshivasai22@gmail.com>
2024-07-11 15:32:02 +0530

    JqueryDemo

---
+++ ClassPractice/msgDemo.js
@@ -0,0 +1,18 @@
+const info=()=>{
+    const name="Shiva";
+    const rollno= 11;
+    return (name + 'has roll no' +rollno);
+}
+export default info;
+
+/*
+    In line export
+    export const name="Shiva"
+    export const age =23
+
+    all at once
+    const name="Shiva"
+    const age =23
+    export {name,age}
+
+    */
